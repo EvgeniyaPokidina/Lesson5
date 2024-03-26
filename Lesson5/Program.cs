74bf2994b301a3c27b35ce287420d3fe510a0379
User0 <User0@DESKTOP-J6BCL92>
2024-03-26 13:01:30 +0300

    Добавьте файлы проекта.

---
+++ Lesson5/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+//Console.Write("Введите количество миллиметров:");// перевод миллиметров в сантиметры
+//double mm = double.Parse(Console.ReadLine());
+//double cm = mm / 10;
+//Console.WriteLine($"В Сантиметрах:{cm}");
+
+//Console.Write("Введите количество миллиметров:");// перевод миллиметров в метры
+//double mm = double.Parse(Console.ReadLine());
+//double m = mm / 1000;
+//Console.WriteLine($"В Метрах:{m}");
+
+//Console.Write("Введите количество миллиметров:");// перевод миллиметров в километры
+//double mm = double.Parse(Console.ReadLine());
+//double km = mm / 1000000;
+//Console.WriteLine($"В Километрах:{km}");
+
+//Console.Write("Введите количество миллиметров:");//перевод миллиметров в мили
+//double mm = double.Parse(Console.ReadLine());
+//double miles = mm / 1609344;
+//Console.WriteLine($"В Милях:{miles:F6}");
+
+Console.Write("Введите количество миллиметров:");//перевод миллиметров в ярды
+double mm = double.Parse(Console.ReadLine());
+double yards = mm / 914.40;
+Console.WriteLine($"В Ярдах:{yards:F6}");
